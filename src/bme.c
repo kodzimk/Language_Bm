01bb2bc2f0f1edb3f41cb4151fff05290d711472
kodzimk <proggamedev@gmail.com>
2025-02-25 21:06:08 +0500

    refactoring all code

--- src/bme.c
+++ src/bme.c
@@ -1,75 +1,80 @@
 #define BM_IMPLEMENTATION
-#include"bm.h"
+#include "./bm.h"
 
-char* shift(int* argc, char*** argv)
+Bm bm = {0};
+
+static  char *shift(int *argc, char ***argv)
 {
     assert(*argc > 0);
-
-    char* result = **argv;
+    char *result = **argv;
     *argv += 1;
     *argc -= 1;
     return result;
 }
 
-int main(int argc, char** argv)
+static void usage(FILE *stream, const char *program)
 {
-    shift(&argc, &argv);
+    fprintf(stream, "Usage: %s -i <input.bm> [-l <limit>] [-h] [-d]\n", program);
+}
 
-    const char* input_file = NULL;
+int main(int argc, char **argv)
+{
+    const char *program = shift(&argc, &argv);
+    const char *input_file_path = NULL;
     int limit = -1;
     int debug = 0;
 
-    while (argc > 0)
-    {
-        const char* flag = shift(&argc, &argv);
-        if (strcmp(flag, "-i") == 0)
-        {
-            if (argc == 0)
-            {
-                fprintf(stdout, "ERROR: n oargument for input file!!! \n");
+    while (argc > 0) {
+        const char *flag = shift(&argc, &argv);
+
+        if (strcmp(flag, "-i") == 0) {
+            if (argc == 0) {
+                usage(stderr, program);
+                fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
                 exit(1);
             }
-            
-            input_file = shift(&argc, &argv);
-        }
-        else if (strcmp(flag, "-l") == 0)
-        {
-            if (argc == 0)
-            {
-                fprintf(stdout, "ERROR: expected value of limit of the program!!! \n");
+
+            input_file_path = shift(&argc, &argv);
+        } else if (strcmp(flag, "-l") == 0) {
+            if (argc == 0) {
+                usage(stderr, program);
+                fprintf(stderr, "ERROR: No argument is provided for flag `%s`\n", flag);
                 exit(1);
             }
 
             limit = atoi(shift(&argc, &argv));
-        }   
-         else if (strcmp(flag, "-d") == 0) 
+        } else if (strcmp(flag, "-h") == 0) {
+            usage(stdout, program);
+            exit(0);
+        } else if (strcmp(flag, "-d") == 0) {
             debug = 1;
-        else
-        {
-            if (argc == 0)
-            {
-                fprintf(stdout, "ERROR: Unkown flag!!!\n");
-                exit(1);
-            }
+        } else {
+            usage(stderr, program);
+            fprintf(stderr, "ERROR: Unknown flag `%s`\n", flag);
+            exit(1);
         }
     }
 
-    bm_load_program_from_file(&bm, input_file);
+    if (input_file_path == NULL) {
+        usage(stderr, program);
+        fprintf(stderr, "ERROR: input was not provided\n");
+        exit(1);
+    }
+
+    bm_load_program_from_file(&bm, input_file_path);
 
     if (!debug) {
         Err err = bm_execute_program(&bm, limit);
-       /* bm_dump_stack(stdout, &bm);*/
+        bm_dump_stack(stdout, &bm);
 
         if (err != ERR_OK) {
             fprintf(stderr, "ERROR: %s\n", err_as_cstr(err));
             return 1;
         }
-    }
-    else {
+    } else {
         while (limit != 0 && !bm.halt) {
-            /*bm_dump_stack(stdout, &bm);*/
-            if (getchar() == 'e')
-                break;
+            bm_dump_stack(stdout, &bm);
+            getchar();
             Err err = bm_execute_inst(&bm);
             if (err != ERR_OK) {
                 fprintf(stderr, "ERROR: %s\n", err_as_cstr(err));
--- src/debasm.c
+++ src/debasm.c
@@ -1,26 +1,28 @@
 #define BM_IMPLEMENTATION
-#include"bm.h"
+#include "./bm.h"
+#include <inttypes.h>
 
-int main(int argc, char** argv)
+Bm bm = {0};
+
+int main(int argc, char *argv[])
 {
     if (argc < 2) {
-        fprintf(stderr, "Usage: ./bm <output.bm>\n");
-        fprintf(stdout, "ERROR: expected input\n");
+        fprintf(stderr, "Usage: ./debasm <input.bm>\n");
+        fprintf(stderr, "ERROR: no input is provided\n");
         exit(1);
     }
 
-  
-    const char* input_file_path = argv[1];
-    if (input_file_path[strlen(input_file_path) - 1] != 'm' && input_file_path[strlen(input_file_path) - 2] != 'b' &&
-        input_file_path[strlen(input_file_path) - 3] != '.')
-    {
-        fprintf(stdout, "ERROR: incorrect format of file\n");
-        exit(1);
+    const char *input_file_path = argv[1];
+
+    bm_load_program_from_file(&bm, input_file_path);
+
+    for (Inst_Addr i = 0; i < bm.program_size; ++i) {
+        printf("%s", inst_name(bm.program[i].type));
+        if (inst_has_operand(bm.program[i].type)) {
+            printf(" %" PRIu64, bm.program[i].operand.as_i64);
+        }
+        printf("\n");
     }
-
-    bm_debasm_file(&bm, input_file_path);
-    
-
 
     return 0;
 }
